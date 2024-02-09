66ad3ba3ae7a51b486ec5e0605ef0767d2aba6b5
marcbraden <43180109+marcbraden@users.noreply.github.com>
2024-02-09 11:26:47 -0600

    Update Speedometer_code.ino

--- Speedometer_code/Speedometer_code.ino
+++ Speedometer_code/Speedometer_code.ino
@@ -4,7 +4,9 @@ int oldTime=0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(2),getTimeInterval,FALLING);
+  attachInterrupt(digitalPinToInterrupt(2),getTimeInterval,FALLING);  //interrupts the code based on input from a pin, immediately executing a function
+  //In this case, everything gets interrupted if the voltage on pin 2 falls from HIGH to LOW, at which point the function getTimeInterval will be executed.
+  //The main loop resumes as soon as getTimeInterval is done.
 }
 
 void loop(){
